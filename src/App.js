48fac5fb8c62fbbb057fcb1680b44d69ad218361
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-02-23 19:56:27 +0100

    footer and dynamic size change for logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header'
 import Banner from'./components/Banner'
 import Gallery from './components/Gallery';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Banner />
       <Gallery />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import Logo from '../assets/Logo.js';
+import '../styles/footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <Logo color='#fff' width='122' height='39.44'/>
+            <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+    )
+}
+
+export default Footer
