31825382ce0e358933e267ef3a833199fcd139bc
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-03-01 14:34:14 +0100

    description and equipments

--- src/routes/rentalPage.jsx
+++ src/routes/rentalPage.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 import Tag from '../components/Tag'
+import Dropdown from '../components/Dropdown';
 import { ReactComponent as Star } from '../assets/star.svg'
 import rentalList from '../datas/logements.json'
 import { useParams } from 'react-router-dom';
@@ -15,17 +16,24 @@ export default function RentalPage() {
     const rating = range.map((rangeElem) =>
                 parseInt(rental.rating, 10) >= rangeElem ? <Star key={rangeElem.toString()} className='star active'/> : <Star key={rangeElem.toString()} className='star'/>
             )
+    const equipments = <ul>{
+                rental.equipments.map((equipment, index) =>
+                    <li key={index}>{equipment}</li>
+                )}
+    </ul>
     return (
         <main>
             < Header />
             <h1>{rental.title}</h1>
             <p>{rental.location}</p>
+            {tags}
             <div>
                 <p>{rental.host.name}</p>
                 <img src={rental.host.picture} alt='portrait of the host'></img>
                 {rating}
-                {tags}
             </div>
+            <Dropdown key={'description'+rental.id} title='Description' content={rental.description} />
+            <Dropdown key={'equipments'+rental.id} title='Équipements' content={equipments} />
             < Footer />
         </main>
   );
