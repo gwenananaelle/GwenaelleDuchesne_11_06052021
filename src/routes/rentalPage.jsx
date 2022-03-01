ccbb65388d08af7b2ca7d3a21e0e63aca584904b
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-03-01 13:13:52 +0100

    rating

--- src/routes/rentalPage.jsx
+++ src/routes/rentalPage.jsx
@@ -1,15 +1,20 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 import Tag from '../components/Tag'
+import { ReactComponent as Star } from '../assets/star.svg'
 import rentalList from '../datas/logements.json'
 import { useParams } from 'react-router-dom';
+import '../styles/rating.css'
 
 export default function RentalPage() {
     let { rentalId } = useParams();
     const rental = rentalList.find(rental => rental.id === rentalId);
     const tags = rental.tags.map( (tag, index) => 
         <Tag key={index+ tag} tag={tag} />)
-
+    const range = [1,2,3,4,5]
+    const rating = range.map((rangeElem) =>
+                parseInt(rental.rating, 10) >= rangeElem ? <Star key={rangeElem.toString()} className='star active'/> : <Star key={rangeElem.toString()} className='star'/>
+            )
     return (
         <main>
             < Header />
@@ -18,6 +23,7 @@ export default function RentalPage() {
             <div>
                 <p>{rental.host.name}</p>
                 <img src={rental.host.picture} alt='portrait of the host'></img>
+                {rating}
                 {tags}
             </div>
             < Footer />
