21acb9276d6a56b24bcdbedb079123bf3088dda0
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-03-20 18:42:33 +0100

    change organisation and fixed a css bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import Header from './components/Header'
 import Banner from './components/Banner'
 import Gallery from './components/Gallery'
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
+import './styles/index.css'
 import App from './App'
-import About from './routes/about'
-import Error from './routes/error'
-import reportWebVitals from './reportWebVitals'
-import RentalPage from './routes/rentalPage'
+import About from './pages/about'
+import Error from './pages/error'
+import RentalPage from './pages/rentalPage'
 
 ReactDOM.render(
     <BrowserRouter>
@@ -21,8 +20,3 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
